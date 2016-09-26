7d588c0da75e038a8606abd7f1a78cb00600a12a
tinmoe <tinmoelintun@gmail.com>
2016-09-26 19:45:36 +0800

    20160926

--- NUSStackup/Controllers/UserController.vb
+++ NUSStackup/Controllers/UserController.vb
@@ -10,17 +10,17 @@ Namespace Controllers
     Public Class UserController
         Inherits BaseApiController
 
-        'GET v1/user/{userid}/{email}
+        'GET v1/user/{userid}/{email} 
         <HttpGet>
         <ActionName("json")>
-        Public Function GetUserDetail(id As Integer, email As String) As HttpResponseMessage
+        Public Function GetUserDetail(id As Integer, emailadddress As String) As HttpResponseMessage
 
-            Dim detail = New UserManager(Configs.ConnectionString).GetUserDetail(id, email)
+            Dim detail = New UserManager(Configs.ConnectionString).GetUserDetail(id, emailadddress)
             If IsNothing(detail) Then
                 Return NUSErrorResponse(HttpStatusCode.NotFound, ErrorStatusCode.NotFound)
             End If
 
-            Dim ehaResponse As New nusResponse(Of Userdetail)(detail)
+            Dim ehaResponse As New NUSResponse(Of UserDetail)(detail)
             Return Request.CreateResponse(HttpStatusCode.OK, ehaResponse)
         End Function
 
