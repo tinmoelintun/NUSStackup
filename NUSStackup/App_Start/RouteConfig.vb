ad2f93251fa680480df3acadb649dce9d98165fa
tinmoelintun <tinmoelintun@gmail.com>
2016-09-26 20:40:44 +0800

    TEST

--- NUSStackup/App_Start/RouteConfig.vb
+++ NUSStackup/App_Start/RouteConfig.vb
@@ -2,5 +2,15 @@
 Imports System.Web.Routing
 
 Public Class RouteConfig
+    Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+
+        'routes.MapRoute( _
+        '    name:="Default", _
+        '    url:="{controller}/{action}/{id}", _
+        '    defaults:=New With {.controller = "Home", .action = "Index", .id = UrlParameter.Optional} _
+        ')
+        routes.MapRoute(name:="Default", url:="index.html")
+    End Sub
 
 End Class
